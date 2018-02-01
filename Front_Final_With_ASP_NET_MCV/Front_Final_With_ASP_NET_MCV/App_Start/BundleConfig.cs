79e5dc669ae138554c4af0dfcbcd15d3cb7936e4
memmedlisakit <memmedlisakit@gmail.com>
2018-02-01 21:55:30 +0400

    home task

---
+++ Front_Final_With_ASP_NET_MCV/Front_Final_With_ASP_NET_MCV/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Front_Final_With_ASP_NET_MCV
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+        }
+    }
+}
---
+++ Front_Final_With_ASP_NET_MCV/Front_Final_With_ASP_NET_MCV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Front_Final_With_ASP_NET_MCV.MvcApplication" Language="C#" %>
